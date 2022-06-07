79bdb2de7ae84df9e2a797bbbd3e1900ea73c255
rhlpandey1 <pandeyra1990@gmail.com>
2022-06-07 22:34:17 +0530

    test

--- src/main/java/org/javaconceptoftheday/strings/PercentageOfUpperLowerAndDigits.java
+++ src/main/java/org/javaconceptoftheday/strings/PercentageOfUpperLowerAndDigits.java
@@ -1,2 +1,19 @@
-package org.javaconceptoftheday.strings;public class PercentageOfUpperLowerAndDigits {
+package org.javaconceptoftheday.strings;
+
+import java.util.Arrays;
+
+public class PercentageOfUpperLowerAndDigits {
+    public static void main(String[] args) {
+        String str="RahUlPandeY";
+        int lowCount=0;
+        int upCount=0;
+        for(int i=0;i<str.length();i++){
+            if(Character.isUpperCase(str.charAt(i)))
+                upCount++;
+            else if(Character.isLowerCase(str.charAt(i)))
+                lowCount++;
+        }
+        double lowPercentage=(lowCount/str.length())*100;
+        System.out.println("lowPercentage = " + lowPercentage);
+    }
 }
