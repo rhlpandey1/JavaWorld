b2c1a51ca67e9651986f802eda0faefd42143e0b
rhlpandey1 <pandeyra1990@gmail.com>
2022-06-07 23:39:53 +0530

    code added

--- src/main/java/org/javaconceptoftheday/strings/PercentageOfUpperLowerAndDigits.java
+++ src/main/java/org/javaconceptoftheday/strings/PercentageOfUpperLowerAndDigits.java
@@ -1,5 +1,6 @@
 package org.javaconceptoftheday.strings;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class PercentageOfUpperLowerAndDigits {
@@ -13,7 +14,8 @@ public class PercentageOfUpperLowerAndDigits {
             else if(Character.isLowerCase(str.charAt(i)))
                 lowCount++;
         }
-        double lowPercentage=(lowCount/str.length())*100;
-        System.out.println("lowPercentage = " + lowPercentage);
+        double lowPercentage=(lowCount*100.0/str.length());
+        DecimalFormat formatter = new DecimalFormat("##.##");
+        System.out.println("lowPercentage = " +formatter.format(lowPercentage));
     }
 }
--- src/main/java/org/javaconceptoftheday/strings/PermutationsOfAString.java
+++ src/main/java/org/javaconceptoftheday/strings/PermutationsOfAString.java
@@ -1,2 +1,42 @@
-package org.javaconceptoftheday.strings;public class PermutationsOfAString {
+package org.javaconceptoftheday.strings;
+
+/**
+ * Write a java program to find all permutations of string recursively.
+ * For example, all permutations of string “JSP” are,
+ * JSP
+ * JPS
+ * SJP
+ * SPJ
+ * PJS
+ * PSJ
+ */
+public class PermutationsOfAString {
+    static void printPermutn(String str, String ans)
+    {
+
+        // If string is empty
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            // ith character of str
+            char ch = str.charAt(i);
+
+            // Rest of the string after excluding
+            // the ith character
+            String ros = str.substring(0, i) +
+                    str.substring(i + 1);
+
+            // Recursive call
+            printPermutn(ros, ans + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+            String str="JSP";
+            printPermutn(str,"");
+    }
 }
