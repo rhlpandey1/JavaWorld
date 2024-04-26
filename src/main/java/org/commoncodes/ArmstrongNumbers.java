b324ee1528b90a82f60d71477e4c55a070a619d1
Rahul <51677360+rhlpandey1@users.noreply.github.com>
2024-04-26 19:47:20 +0530

    Update ArmstrongNumbers.java

--- src/main/java/org/commoncodes/ArmstrongNumbers.java
+++ src/main/java/org/commoncodes/ArmstrongNumbers.java
@@ -13,6 +13,12 @@ public class ArmstrongNumbers {
              flag=true;
         return flag;
     }
+    private static boolean isArmStrongUsingJava8(int n){
+        int len=String.valueOf(n).length();
+        int sum=String.valueOf(n).chars().map(ch->Character.digit(ch,10))
+                .map(digit->(int)Math.pow(digit,len)).sum();
+        return sum==n;
+    }
     public static void main(String[] args) throws Exception {
         //Armstrong numbers : sum of 3 to the power of all the digits should be same as the number
         System.out.println(isArmStrong(111));
